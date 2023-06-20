7041a2e51376647628af077b4654fb6195391610
EllaKaye <ella.kaye@gmail.com>
2023-06-20 12:36:39 +0100

    Explain purpose of debug_practice_workthrough

--- solutions/debug_practice_workthrough.R
+++ solutions/debug_practice_workthrough.R
@@ -1,7 +1,11 @@
+# buggy function
+# edit this file as demo during workshop to see changes develop
+
 f <- function(n){
-  x <- sample(1:2*n)
-  y <- matrix(x, 2, n)
-  z <- matrix(x, n, 2)
+  x <- sample(1:n)
+  y <- matrix(x, 2, n/2)
+  z <- matrix(x, n/2, 2)
   y.z <- (y %*% z)
-  apply(y.z, 1, mean)
+  apply(yz, mean, 1)
 }
+
